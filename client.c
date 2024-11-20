969155100f47ace7493b671a516dda78a11e33aa
sharvanich <130927204+sharvanich@users.noreply.github.com>
2024-11-20 06:34:54 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,1 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
+#define PORT 8080
+#define MAX_CLIENTS 3
+#define BUFFER_SIZE 1024
+
+void *client_handler(void *socket) {
+    int client_socket = *(int *)socket;
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    while (1) {
+        bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0';
+            printf("Server: Message received from Client: %s\n", buffer);
+
+            send(client_socket, buffer, strlen(buffer), 0);
+            printf("Server: Message delivered to Client\n");
+        } 
+        
+        else if (bytes_received == 0) {
+            printf("Server: Client disconnected.\n");
+            close(client_socket);
+            break;
+        }
+    }
+
+    return NULL;
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    pthread_t client_thread;
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 5) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server: Listening on port %d...\n", PORT);
+
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+        if (client_socket < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        printf("Server: Client connected.\n");
+
+        pthread_create(&client_thread, NULL, client_handler, &client_socket);
+    }
+
+    close(server_socket);
+    return 0;
+}
